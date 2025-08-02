f97c8ba06fb57839a028e33f4d3e66d6850b3138
saki0411 <chipdale411@gmail.com>
2025-08-02 18:29:02 +0900

    update .gitignore

---
+++ ios/KUYOU/KUYOUApp.swift
@@ -0,0 +1,17 @@
+//
+//  KUYOUApp.swift
+//  KUYOU
+//
+//  Created by saki on 2025/08/02.
+//
+
+import SwiftUI
+
+@main
+struct KUYOUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
